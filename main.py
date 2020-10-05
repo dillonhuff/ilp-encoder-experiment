671e812f8aa8ba6ef453432550b7d7280352ca37
Dillon Huff <dillonhuff@gmail.com>
2020-10-04 20:17:08 -0700

    added negation test

--- main.py
+++ main.py
@@ -57,9 +57,10 @@ class ILPBuilder:
         self.variable_bounds = {}
         self.objective = None
 
-    def unique_id(self, prefix='U_'):
+    def unique_name(self, prefix='U_'):
         un = self.unique_num
         self.unique_num += 1
+        print('unique num =', self.unique_num)
         return prefix + str(un);
 
     def add_outer_forall(self, var, lb, ub):
@@ -77,6 +78,7 @@ class ILPBuilder:
         # self.add_constraint(name + ' = ' + expr)
         
     def add_int_var(self, name, lower=None, upper=None):
+        assert(not name in self.variables)
         self.variables.append(name)
         self.variable_bounds[name] = (lower, upper)
         if (upper != None):
@@ -91,11 +93,9 @@ class ILPBuilder:
 
     def add_constraint_gez(self, cst):
         self.constraints.append(Constraint(cst, '>='))
-        # self.constraints.append(cst + ' >= 0')
 
     def add_constraint_lez(self, cst):
         self.constraints.append(Constraint(cst, '<='))
-        # self.constraints.append(cst + ' <= 0')
 
     def set_objective(self, obj):
         self.objective = obj
@@ -103,15 +103,15 @@ class ILPBuilder:
     def is_zero_one_var(self, name):
         return self.variable_bounds[name][0] == 0 and self.variable_bounds[name][1] == 1
 
-    def fresh_zero_one_var(self):
+    def fresh_zero_one_var(self, prefix='zo_'):
         name = self.unique_name('zo_')
         self.add_int_var(name, 0, 1)
         return name
 
     def negate(self, var):
         assert(self.is_zero_one_var(var))
-        neg = self.fresh_zero_one_var(self)
-        self.synonym(neg, '1 - {0}'.format(var))
+        neg = self.fresh_zero_one_var('neg_')
+        self.add_constraint_eqz('{0} - 1 + {1}'.format(neg, var))
         return neg
 
     def add_indicator(self, target, ub):
@@ -132,7 +132,8 @@ class ILPBuilder:
             problem = add_constraint('c{0}'.format(i), str(c), problem)
             i += 1
 
-        problem += 'minimize obj : {0};\n'.format(self.objective)
+        if self.objective != None:
+            problem += 'minimize obj : {0};\n'.format(self.objective)
         problem += 'solve;\n'
 
         i = 0
@@ -150,49 +151,59 @@ class ILPBuilder:
         return val_map
 
 builder = ILPBuilder()
-
-# Schedule parameters
-builder.add_int_var("ii_p", 1, 100000)
-builder.add_int_var("ii_c", 1, 100000)
-
-builder.add_int_var("d_p", 0, 100000)
-builder.add_int_var("d_c", 0, 100000)
-
-# Resource assignment constraints
-builder.add_int_var("unit_p", 0, 1)
-builder.add_int_var("unit_c", 0, 1)
-
-# These indicator variables sum to zero iff
-# p and c are using the same functional unit
-builder.add_synonym("neg_p_c_share", "unit_p - unit_c")
-ub = 1
-lb = -1
-builder.add_indicator("neg_p_c_share", ub)
-
-builder.add_synonym("neg_c_p_share", "-1*neg_p_c_share")
-builder.add_indicator("neg_c_p_share", -1*lb)
-
-# These indicator variables sum to zero
-# iff p and c are scheduled at the same time
-# builder.add_synonym("neg_p_c_time", "ii_c*c + d_c - ii_p*p - d_p")
-builder.add_synonym("neg_p_c_time", "ii_c + d_c - ii_p - d_p")
-ub = builder.ub("ii_c")*builder.ub("d_c")
-lb = -1*builder.lb("ii_p")*builder.ub("d_p")
-
-builder.add_indicator("neg_p_c_time", ub)
-
-builder.add_synonym("neg_c_p_time", "-1*neg_p_c_time")
-builder.add_indicator("neg_c_p_time", -1*lb)
-
-builder.set_objective('ii_p + ii_c')
-
-# Iteration domains
-builder.add_outer_forall("p", 1, 10)
-builder.add_outer_forall("c", 1, 10)
-
-builder.add_constraint_gez("I_neg_p_c_share + I_neg_c_p_share + I_neg_p_c_time + I_neg_c_p_time - 1")
-
+builder.add_int_var('a', 0, 1)
+builder.add_constraint_eqz('a - 1')
+na = builder.negate('a')
 sol = builder.solve()
 for s in sol:
     print('\t', s, '=', sol[s])
+assert(sol['a'] == 1)
+assert(sol[na] == 0)
+
+
+# builder = ILPBuilder()
+# # Schedule parameters
+# builder.add_int_var("ii_p", 1, 100000)
+# builder.add_int_var("ii_c", 1, 100000)
+
+# builder.add_int_var("d_p", 0, 100000)
+# builder.add_int_var("d_c", 0, 100000)
+
+# # Resource assignment constraints
+# builder.add_int_var("unit_p", 0, 1)
+# builder.add_int_var("unit_c", 0, 1)
+
+# # These indicator variables sum to zero iff
+# # p and c are using the same functional unit
+# builder.add_synonym("neg_p_c_share", "unit_p - unit_c")
+# ub = 1
+# lb = -1
+# builder.add_indicator("neg_p_c_share", ub)
+
+# builder.add_synonym("neg_c_p_share", "-1*neg_p_c_share")
+# builder.add_indicator("neg_c_p_share", -1*lb)
+
+# # These indicator variables sum to zero
+# # iff p and c are scheduled at the same time
+# # builder.add_synonym("neg_p_c_time", "ii_c*c + d_c - ii_p*p - d_p")
+# builder.add_synonym("neg_p_c_time", "ii_c + d_c - ii_p - d_p")
+# ub = builder.ub("ii_c")*builder.ub("d_c")
+# lb = -1*builder.lb("ii_p")*builder.ub("d_p")
+
+# builder.add_indicator("neg_p_c_time", ub)
+
+# builder.add_synonym("neg_c_p_time", "-1*neg_p_c_time")
+# builder.add_indicator("neg_c_p_time", -1*lb)
+
+# builder.set_objective('ii_p + ii_c')
+
+# # Iteration domains
+# builder.add_outer_forall("p", 1, 10)
+# builder.add_outer_forall("c", 1, 10)
+
+# builder.add_constraint_gez("I_neg_p_c_share + I_neg_c_p_share + I_neg_p_c_time + I_neg_c_p_time - 1")
+
+# sol = builder.solve()
+# for s in sol:
+    # print('\t', s, '=', sol[s])
 
